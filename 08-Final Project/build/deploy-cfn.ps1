fa2e4ff100a4bdb9c4bbbc9d6c5ef8e568e536c2
Trevor Sullivan <trevor@trevorsullivan.net>
2020-08-03 18:38:50 -0600

    Update deployment script for final solution

--- 08-Final Project/build/deploy-cfn.ps1
+++ 08-Final Project/build/deploy-cfn.ps1
@@ -1,3 +1,5 @@
+Set-DefaultAWSRegion -Region us-west-1 
+
 $StackName = 'serverless-video-processor'
 $TemplateBody = Get-Content -Raw -Path $PSScriptRoot/../cloudformation.yml
 
@@ -5,13 +7,32 @@ try {
     Update-CFNStack -StackName $StackName -TemplateBody $TemplateBody -Capability CAPABILITY_NAMED_IAM -ErrorAction Stop
 }    
 catch {
-    throw $PSItem
+
+    if ($PSItem.Exception.Message -match 'ROLLBACK_COMPLETE') {
+        Write-Host -Object 'Removing CloudFormation stack' -ForegroundColor Red
+
+        Remove-S3Bucket -BucketName cbt-trevor-cloudtrail-usw2  -DeleteBucketContent -Force
+        Remove-S3Bucket -BucketName cbt-video-processed  -DeleteBucketContent -Force
+        Remove-S3Bucket -BucketName cbt-video-sink  -DeleteBucketContent -Force
+        
+        Remove-CFNStack -StackName $StackName -Force
+        Wait-CFNStack -StackName $StackName -Status DELETE_COMPLETE
+        $CreateStack = $true
+    }
+    if ($PSItem.Exception.Message -match 'does not exist') {
+        $CreateStack = $true
+    }
+    else {
+        throw $PSItem
+    }
+
+    if ($CreateStack) {
+        New-CFNStack -StackName $StackName -TemplateBody $TemplateBody -Capability CAPABILITY_NAMED_IAM
+    }
     # Remove-CFNStack -StackName $StackName -Force -ErrorAction Ignore
     # Remove-S3Bucket -DeleteBucketContent -Force -BucketName cbt-trevor-cloudtrail-usw2
     # Remove-S3Bucket -DeleteBucketContent -Force -BucketName cbt-video-sink
     # Wait-CFNStack -StackName $StackName -Status DELETE_COMPLETE
-
-    # New-CFNStack -StackName $StackName -TemplateBody $TemplateBody -Capability CAPABILITY_NAMED_IAM
 }
 
 return
