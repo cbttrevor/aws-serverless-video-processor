3fc13bf90ed4348887db939a57679792cee917ed
Trevor Sullivan <trevor@trevorsullivan.net>
2020-08-03 18:39:03 -0600

    Update app script for final solution

--- 08-Final Project/processor/convert.ps1
+++ 08-Final Project/processor/convert.ps1
@@ -1,13 +1,12 @@
-# Entry point for video conversion
-
+# This file is the container entry point for video conversion
+$DestinationBucket = 'cbt-video-processed'
 
 Write-Host $env:S3Key
 $env:S3Key
 
-$DestinationBucket = ''
-Read-S3Object -BucketName $env:S3Bucket -S3Object $env:S3Key -File video.mp4
+Read-S3Object -BucketName $env:S3Bucket -Key $env:S3Key -File video.mp4
 
 # Perform video conversion
-ffmpeg -i input.mp4 output.mp4
+ffmpeg -i video.mp4 output.webm
 
-Write-S3Object -BucketName $env:DestinationBucket
+Write-S3Object -BucketName $DestinationBucket -File output.webm
